abcac18869c5b6082b45ce88c27accad201ea03d
4FViP <152526348+4FViP@users.noreply.github.com>
2024-02-08 03:55:32 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = int(os.environ.get("TG_BOT_TOKEN", "5901305358:AAG_DA1t3CXnakCe8BAEsNWGG5uDU8MtpJk"))
-
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", " ")
+    
     APP_ID = int(os.environ.get("APP_ID", 11750778))
 
     API_HASH = os.environ.get("API_HASH", "d0352df3ddb5e00bcf16b55dae071b52")
